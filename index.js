4ae9e548abed560b18700bce7e2e686452456e14
thrilliant <hello@thrilliant.com.ng>
2022-12-26 14:08:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const request = require('request');
+const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -12,55 +12,88 @@ fs.readFile('./nowplaying.txt', 'utf8', (err, data) => {
   }
 
   // Send a request to the Genius lyrics API with the contents of the "nowplaying.txt" file
-  request.get({
-    url: `https://api.genius.com/search?q=${encodeURIComponent(data)}`,
+  const options = {
+    hostname: 'api.genius.com',
+    path: `/search?q=${encodeURIComponent(data)}`,
     headers: {
-      'Authorization': `jwfNX8M9XHX8pec12_C4LFqCuKXCnxw1UFvLRVuMtsbwsYf5lInHVQALG482_V6hQFKY9w6d1WdsaJV42nhAoA`,
+      'Authorization': `Bearer YOUR_ACCESS_TOKEN`,
     },
-  }, (error, response, body) => {
-    if (error) {
-      console.error(`Error sending request to Genius lyrics API: ${error.message}`);
-      return;
-    }
+  };
 
-    // Parse the API response as JSON
-    const json = JSON.parse(body);
-
-    // Get the first song from the API response
-    const song = json.response.hits[0].result;
-
-    // Get the song title and artist
-    const title = song.title;
-    const artist = song.primary_artist.name;
-
-    // Get the lyrics for the song
-    const lyrics = json.response.song.lyrics;
-
-    // Send a request to the iTunes Search API to get the cover art for the song
-    request.get({
-      url: `https://itunes.apple.com/search?term=${encodeURIComponent(`${title} ${artist}`)}&entity=song`,
-      json: true,
-    }, (error, response, body) => {
-      if (error) {
-        console.error(`Error sending request to iTunes Search API: ${error.message}`);
+  const req = http.get(options, (res) => {
+    let body = '';
+    res.on('data', (chunk) => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      if (res.statusCode !== 200) {
+        console.error(`Error sending request to Genius lyrics API: status code ${res.statusCode}`);
         return;
       }
 
-      // Get the cover art URL for the first result
-      const coverArtUrl = body.
-results[0].artworkUrl100;
+      // Parse the API response as JSON
+      const json = JSON.parse(body);
 
-      // Set up an endpoint to display the song title, artist, cover art, and lyrics
-      app.get('/lyrics', (req, res) => {
-        res.send(`
-          <img src="${coverArtUrl}" alt="Cover art for ${title}">
-          <h1>${title}</h1>
-          <h2>by ${artist}</h2>
-          <pre>${lyrics}</pre>
-        `);
-      });
-    });
+      // Get the first song from the API response
+      const song = json.response.hits[0].result;
+
+      // Get the song title and artist
+      const title = song.title;
+      const artist = song.primary_artist.name;
+
+      // Get the lyrics for the song
+      const lyrics = json.response.song.lyrics;
+
+      // Send a request to the iTunes Search API to get the cover art for the song
+      const options = {
+        hostname: 'itunes.apple.com',
+        path: `/search?term=${encodeURIComponent(`${title} ${artist}`)}&entity=song`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const req = http.get(options, (res) => {
+        let body = '';
+        res.on('data', (chunk) => {
+          body += chunk;
+        });
+        res.on('end', () => {
+          if (res.statusCode !== 200) {
+            console.error(`Error sending request to iTunes Search API: status code ${res.statusCode}`);
+            return;
+          }
+
+          // Get the cover art URL for the first result
+          let coverArtUrl = JSON.parse(body).results[0].artworkUrl100;
+
+          // If the iTunes Search API did not return a cover art URL, use the Last.fm API as a backup
+          if (!coverArtUrl) {
+            const options = {
+  hostname: 'ws.audioscrobbler.com',
+  path: `/2.0/?method=track.getInfo&api_key=YOUR_LAST_FM_API_KEY&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(title)}&format=json`,
+};
+    url: `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=YOUR_LAST_FM_API_KEY&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(title)}&format=json`,
+    json: true,
+  }, (error, response, body) => {
+    if (error) {
+      console.error(`Error sending request to Last.fm API: ${error.message}`);
+      return;
+    }
+
+    // Get the cover art URL from the Last.fm API response
+    coverArtUrl = body.track.album.image[3]['#text'];
   });
+}
+
+// Set up an endpoint to display the song title, artist, cover art, and lyrics
+app.get('/lyrics', (req, res) => {
+  res.send(`
+    <h1>${title}</h1>
+    <h2>by ${artist}</h2>
+    <img src="${coverArtUrl}" alt="Cover art for ${title}">
+    <pre>${lyrics}</pre>
+  `);
 });
 
 // Start the server
